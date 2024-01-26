b3acd27473ed9b5c45bddfe83416befbe62f736a
Frédéric Royet <frederic.royet@logipro.com>
2024-01-26 12:07:07 +0100

    Initialisation Espace de travail FizzBuzz

---
+++ tests/FizzBuzzTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace FizzBuzz\Tests;
+
+use FizzBuzz\FizzBuzz;
+use PHPUnit\Framework\TestCase;
+
+class FizzBuzzTest extends TestCase
+{
+    public function testFizzBuzz(): void
+    {
+        $FizzBuzz = new FizzBuzz();
+    }
+}
