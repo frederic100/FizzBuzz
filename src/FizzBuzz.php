b9dbda93509f5302360361feec3784d1217468e2
Frédéric Royet <frederic.royet@logipro.com>
2024-01-29 15:45:43 +0100

    FizzBuzz Ok

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -4,8 +4,36 @@ namespace FizzBuzz;
 
 class FizzBuzz
 {
+    public const SEPARATEUR = ", ";
+    public const MULTIPLE_FIZZ = 3;
+    public const MULTIPLE_BUZZ = 5;
+    public const FIZZ = "Fizz";
+    public const BUZZ = "Buzz";
     public function generateFizzBuzz(int $nombre): string
     {
-        return (string)$nombre;
+        $result = "";
+
+        for ($i = 1; $i < $nombre; $i++) {
+            $result .= $this->getFizzBuzz($i) . self::SEPARATEUR;
+        }
+        $result .= $this->getFizzBuzz($i);
+        return $result;
+    }
+
+    private function getFizzBuzz(int $nombre): string
+    {
+        $resultatNombre = "";
+
+        if ($nombre % self::MULTIPLE_FIZZ == 0) {
+            $resultatNombre .= self::FIZZ;
+        }
+        if ($nombre % self::MULTIPLE_BUZZ == 0) {
+            $resultatNombre .=  self::BUZZ;
+        }
+        if (empty($resultatNombre)) {
+            $resultatNombre .= strval($nombre);
+        }
+
+        return $resultatNombre;
     }
 }
--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -7,10 +7,51 @@ use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
+    public FizzBuzz $fizzBuzz;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->fizzBuzz = new FizzBuzz();
+    }
     public function testFizzBuzz(): void
     {
-        $fizzBuzz = new FizzBuzz();
-        $resultat = $fizzBuzz->generateFizzBuzz(1);
+        $resultat = $this->fizzBuzz->generateFizzBuzz(1);
         $this->assertEquals("1", $resultat);
+    }
+    public function testFizzBuzz2(): void
+    {
+        $resultat = $this->fizzBuzz->generateFizzBuzz(2);
+        $this->assertEquals("1, 2", $resultat);
+    }
+    public function testFizzBuzz3(): void
+    {
+        $resultat = $this->fizzBuzz->generateFizzBuzz(3);
+        $this->assertEquals("1, 2, Fizz", $resultat);
+    }
+    public function testFizzBuzz4(): void
+    {
+        $resultat = $this->fizzBuzz->generateFizzBuzz(4);
+        $this->assertEquals("1, 2, Fizz, 4", $resultat);
+    }
+    public function testFizzBuzz5(): void
+    {
+        $resultat = $this->fizzBuzz->generateFizzBuzz(5);
+        $this->assertEquals("1, 2, Fizz, 4, Buzz", $resultat);
+    }
+    public function testFizzBuzz8(): void
+    {
+        $resultat = $this->fizzBuzz->generateFizzBuzz(8);
+        $this->assertEquals("1, 2, Fizz, 4, Buzz, Fizz, 7, 8", $resultat);
+    }
+    public function testFizzBuzz15(): void
+    {
+        $resultat = $this->fizzBuzz->generateFizzBuzz(15);
+        $this->assertEquals("1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz", $resultat);
+    }
+    public function testFizzBuzz30(): void
+    {
+        $resultat = $this->fizzBuzz->generateFizzBuzz(30);
+        $this->assertEquals("1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16, 17, Fizz, " .
+            "19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, FizzBuzz", $resultat);
     }
 }
