695b5419e57e83aa19901cf65e9eed8b731bf0c3
Frédéric Royet <frederic.royet@logipro.com>
2024-02-08 16:21:21 +0100

    Add FizzBuzz log for error

--- src/Exceptions/NegativeNotAllowedException.php
+++ src/Exceptions/NegativeNotAllowedException.php
@@ -9,11 +9,13 @@ use function Safe\file_put_contents;
 class NegativeNotAllowedException extends Exception
 {
     public const MESSAGE_ERREUR = "'%d' seul un nombre strictement positif est autorisé";
+    public const EOL = "\n";
+
     public function __construct(int $badnumber)
     {
         $message = sprintf(self::MESSAGE_ERREUR, $badnumber);
         parent::__construct($message);
         $fileName = __DIR__ . "/../../log/Exceptions.log";
-        file_put_contents($fileName, $message);
+        file_put_contents($fileName, $message . self::EOL, FILE_APPEND);
     }
 }
--- tests/Exceptions/NegativeNotAllowedTest.php
+++ tests/Exceptions/NegativeNotAllowedTest.php
@@ -4,13 +4,38 @@ namespace FizzBuzz\Tests\Exceptions;
 
 use FizzBuzz\Exceptions\NegativeNotAllowedException;
 use PHPUnit\Framework\TestCase;
+use SebastianBergmann\LinesOfCode\NegativeValueException;
 
 class NegativeNotAllowedTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        unlink(__DIR__ . '/../../log/Exceptions.log');
+    }
+
     public function testWriteLog(): void
     {
         new NegativeNotAllowedException(-1);
         $content = file_get_contents(__DIR__ . "/../../log/Exceptions.log");
-        $this->assertEquals("'-1' seul un nombre strictement positif est autorisé", $content);
+        $this->assertEquals("'-1' seul un nombre strictement positif est autorisé" .
+            NegativeNotAllowedException::EOL, $content);
+    }
+
+    public function testWriteLog2(): void
+    {
+        new NegativeNotAllowedException(0);
+        $content = file_get_contents(__DIR__ . "/../../log/Exceptions.log");
+        $this->assertEquals("'0' seul un nombre strictement positif est autorisé" .
+            NegativeNotAllowedException::EOL, $content);
+    }
+
+    public function testWriteLogDeuxErreurs(): void
+    {
+        new NegativeNotAllowedException(0);
+        new NegativeNotAllowedException(-1);
+        $content = file_get_contents(__DIR__ . "/../../log/Exceptions.log");
+        $this->assertEquals("'0' seul un nombre strictement positif est autorisé" .
+            NegativeNotAllowedException::EOL . "'-1' seul un nombre strictement positif est autorisé" .
+            NegativeNotAllowedException::EOL, $content);
     }
 }
